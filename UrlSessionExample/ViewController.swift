83c4f95424b27af636ce110df722e3246403a4e0
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2018-10-29 11:04:56 -0600

    fist commit

--- UrlSessionExample/ViewController.swift
+++ UrlSessionExample/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var request : URLRequest!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = "https://api.bitso.com/v3/ticker/?book=eth_mxn"
+        self.generateRequest(url: url)
+    }
+    
+    
+    func generateRequest(url: String){
+        guard let endpointUrl = URL(string: url) else {
+            return
+        }
+        
+        request = URLRequest(url: endpointUrl)
+        request.httpMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+      
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            do {
+                let result = try JSONSerialization.jsonObject(with: data!, options: []) as? [String:AnyObject]
+                
+                print("Result",result!)
+                
+            } catch {
+                print("Error -> \(error)")
+            }
+        }
+        
+        
+        task.resume()
+        
+        
+        
     }
 
 
