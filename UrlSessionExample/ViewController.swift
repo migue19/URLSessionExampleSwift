a5d605c5113132a2399894ffb1c110cddfe4e5a0
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2018-10-29 10:44:11 -0600

    Initial Commit

---
+++ UrlSessionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UrlSessionExample
+//
+//  Created by Miguel Mexicano Herrera on 10/29/18.
+//  Copyright © 2018 Miguel Mexicano Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
